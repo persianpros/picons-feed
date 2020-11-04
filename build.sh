58660d5eeee57cce8b1c1f48f8df7716cf707160
Persian Prince <persianpros@yahoo.com>
2020-11-04 20:09:18 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ commit_files() {
 }
 
 upload_files() {
-  git remote add upstream https://${GH_TOKEN}@github.com/persianpros/picons-feed.git > /dev/null 2>&1
+  git remote add upstream https://${GH_TOKEN}@github.com/OpenVisionE2/picons-feed.git > /dev/null 2>&1
   git push --quiet upstream master || echo "failed to push with error $?"
 }
 
