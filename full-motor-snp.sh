719b31dfe64c4d326d8600176b708c74078b3ca5
persianpros <persianpros@gmail.com>
2020-11-05 00:59:03 +0330

    We can't upload more than 100MB to github

--- full-motor-snp.sh
+++ full-motor-snp.sh
@@ -2,3 +2,6 @@
 
 ./fetch-ipk.sh https://openpicons.com/picons/?dir=full-motor-snp
 
+# We may can't upload enigma2-plugin-picons-snp-full.800x450-760x410.light.on.transparent because of github's 100MB limit per file
+find . -name '*.ipk' -size +98M | xargs rm -f
+
--- full-motor-srp.sh
+++ full-motor-srp.sh
@@ -2,3 +2,5 @@
 
 ./fetch-ipk.sh https://openpicons.com/picons/?dir=full-motor-srp
 
+# We may can't upload enigma2-plugin-picons-srp-full.800x450-760x410.light.on.transparent because of github's 100MB limit per file
+find . -name '*.ipk' -size +98M | xargs rm -f
